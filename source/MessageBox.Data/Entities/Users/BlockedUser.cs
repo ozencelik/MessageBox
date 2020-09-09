1ac1bd417b43a4d807d0679e87eb0344b074cbd1
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2020-09-09 23:06:03 +0300

    Comment added into BlockedUser objects.

--- source/MessageBox.Data/Entities/Users/BlockedUser.cs
+++ source/MessageBox.Data/Entities/Users/BlockedUser.cs
@@ -6,11 +6,13 @@ namespace MessageBox.Data.Entities
     {
         /// <summary>
         /// BlockingUserId as foreign key.
+        /// This user blocked another user.
         /// </summary>
         public int BlockingUserId { get; set; }
 
         /// <summary>
         /// BlockedUserId  as foreign key.
+        /// This user blocked by another user.
         /// </summary>
         public int BlockedUserId { get; set; }
     }
